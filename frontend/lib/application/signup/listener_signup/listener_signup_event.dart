d8deed9d23849ece1003177b34fbc8994d7ca141
Mati Milkessa <matimilkessa@gmail.com>
2024-05-25 23:56:11 +0300

    .

--- frontend/lib/application/signup/listener_signup/listener_signup_event.dart
+++ frontend/lib/application/signup/listener_signup/listener_signup_event.dart
@@ -1,5 +1,4 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart' as flutter;
 import '../../../domain/signup/listener.dart';
 
 abstract class SignupEvent extends Equatable {
