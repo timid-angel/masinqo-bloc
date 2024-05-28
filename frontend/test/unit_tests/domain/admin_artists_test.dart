90fdcd100a68ce109b92d99d4f274c4c665629d5
Natanim <kemalnatanim@gmail.com>
2024-05-28 23:01:57 +0300

    admin_artist unit tests

---
+++ frontend/test/unit_tests/domain/admin_artists_test.dart
@@ -0,0 +1,63 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:masinqo/domain/admin/admin_artists/admin_artists.dart';
+import 'package:masinqo/infrastructure/admin/admin_artists/admin_artists_repository.dart';
+import 'package:masinqo/infrastructure/admin/admin_artists/admin_artists_dto.dart';
+import 'package:masinqo/domain/admin/admin_failure.dart';
+import 'package:masinqo/domain/admin/admin_success.dart';
+import 'package:masinqo/infrastructure/admin/admin_artists/admin_artists_success.dart';
+import 'package:masinqo/infrastructure/admin/admin_artists/admin_artists_failures.dart';
+
+class MockAdminArtistRepository extends Mock implements AdminArtistsRepository {
+  @override
+  Future<Either<ArtistDeleteFailure, ArtistDeleteSuccess>> deleteArtist(
+      ArtistID id) {
+    return super.noSuchMethod(
+      Invocation.method(#deleteArtist, [id]),
+      returnValue: Future.value(Right(ArtistDeleteSuccess())),
+      returnValueForMissingStub: Future.value(Right(ArtistDeleteSuccess())),
+    );
+  }
+}
+
+void main() {
+  group('AdminArtistsCollection', () {
+    final mockAdminArtistsRepository = MockAdminArtistRepository();
+    final adminArtistsCollection = AdminArtistsCollection(
+        adminArtistsRepository: mockAdminArtistsRepository);
+
+    test('getArtists returns AdminFailure when token is empty', () async {
+      when(mockAdminArtistsRepository.token).thenReturn('');
+
+      final result = await adminArtistsCollection.getArtists();
+      expect(
+          result.fold((l) => l.message, (r) => null), equals("Invalid token"));
+    });
+
+    test('deleteArtist returns AdminFailure when token is empty', () async {
+      when(mockAdminArtistsRepository.token).thenReturn('');
+
+      final result = await adminArtistsCollection.deleteArtist('testId');
+      expect(
+          result.fold((l) => l.message, (r) => null), equals("Invalid token"));
+    });
+
+    test('changeStatus returns AdminFailure when token is empty', () async {
+      when(mockAdminArtistsRepository.token).thenReturn('');
+
+      final result = await adminArtistsCollection.changeStatus('testId', true);
+      expect(
+          result.fold((l) => l.message, (r) => null), equals("Invalid token"));
+    });
+    test('getArtists returns GetArtistsSuccess when token is not empty',
+        () async {
+      when(mockAdminArtistsRepository.token).thenReturn('validToken');
+      when(mockAdminArtistsRepository.getArtists())
+          .thenAnswer((_) async => Right(ArtistFetchSuccess(artists: [])));
+
+      final result = await adminArtistsCollection.getArtists();
+      expect(result, isA<Right<AdminFailure, GetArtistsSuccess>>());
+    });
+  });
+}
